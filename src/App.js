57dfe72268bd89a08fbd6054364ba1ed05591f3e
Pia Fåk Sunnanbo <frusunnanbo@users.noreply.github.com>
2021-07-24 17:09:03 +0200

    Create React app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import pia from './pia.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={pia} alt="pia" />
+        <p>
+          Hej hej
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
