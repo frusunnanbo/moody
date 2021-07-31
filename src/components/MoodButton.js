043550dd2d3db52dac835f354973839141bd71f6
Pia Fåk Sunnanbo <frusunnanbo@users.noreply.github.com>
2021-07-31 18:53:47 +0200

    Color changing buttons

--- src/components/MoodButton.js
+++ src/components/MoodButton.js
@@ -2,18 +2,28 @@ import React, { useState } from 'react';
 import styled from 'styled-components'
 
 const StyledButton = styled.button`
-    height: 100px;
-    width: 100px;
+        height: 100px;
+        width: 100px;
+        border-radius: 10px;
+        margin: 20px; 
 `
 
-function MoodButton({ text }) {
+function MoodButton({ text, baseColor }) {
     const [intensity, setIntensity] = useState(0);
 
-    const onClick = () => {
+    const increaseIntensity = () => {
         setIntensity(intensity + 1);
     }
 
-    return <StyledButton onClick={onClick}>{text} intensity: { intensity }</StyledButton>
+    const backgroundColor = (intensity) => {
+        return `rgba(${baseColor.r}, ${baseColor.g}, ${baseColor.b}, ${intensity * 0.01})`;
+    }
+
+    return <StyledButton
+        onClick={increaseIntensity}
+        style={{ backgroundColor: backgroundColor(intensity) }}>
+        {text}
+    </StyledButton>
 }
 
 export default MoodButton;
