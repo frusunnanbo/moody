e9b4d85a660112416dc05bf7a13e19279c378557
Pia Fåk Sunnanbo <frusunnanbo@users.noreply.github.com>
2021-09-05 19:43:37 +0200

    Active monitor checks part of precondition

--- systemtest/server.js
+++ systemtest/server.js
@@ -3,8 +3,12 @@ const express = require('express');
 
 const app = express();
 
+const EVERY_FIVE_MINUTES = 5 * 60 * 1000;
+
 app.get('/status', function (req, res) {
     return res.send(status.get());
 });
+
+status.checkAtInterval(EVERY_FIVE_MINUTES);
 
 app.listen(process.env.PORT || 8080);
--- systemtest/status.js
+++ systemtest/status.js
@@ -1,6 +1,35 @@
+const fetch = require('node-fetch');
+
+const url = "https://moody.frusunnanbo.se"
+const testRoomPath = "/api/rooms/test";
+
+const status = {
+    precondition: "ok",
+    increase: "ok",
+    autodecrease: "ok",
+};
+
+function checkAtInterval(milliseconds) {
+    checkStatus();
+    setInterval(checkStatus, milliseconds);
+}
+
+async function checkStatus() {
+    console.log("Checking status");
+    status.precondition = await checkPrecondition();
+}
+
+async function checkPrecondition() {
+    const moods = await fetch(url + testRoomPath)
+            .then(response => response.json());
+    console.log(JSON.stringify(moods));
+    if (moods.happy !== 0) {
+        return `Expected happy mood to be 0, found ${moods.happy}`;
+    }
+    return "ok";
+}
+
 module.exports = {
-    get: () => ({
-        increase: true,
-        autodecrease: true
-    })
+    checkAtInterval,
+    get: () => status
 };
