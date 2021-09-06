ffc249d5f132f2f6e84a0628200fe28b2f85492b
Pia Fåk Sunnanbo <frusunnanbo@users.noreply.github.com>
2021-09-06 21:08:37 +0200

    Some sort of check

--- systemtest/status.js
+++ systemtest/status.js
@@ -1,32 +1,42 @@
 const fetch = require('node-fetch');
+const assert = require('assert');
 
 const url = "https://moody.frusunnanbo.se"
 const testRoomPath = "/api/rooms/test";
 
 const status = {
-    precondition: "ok",
     increase: "ok",
     autodecrease: "ok",
 };
 
 function checkAtInterval(milliseconds) {
-    checkStatus();
+    checkStatus()
     setInterval(checkStatus, milliseconds);
 }
 
 async function checkStatus() {
     console.log("Checking status");
-    status.precondition = await checkPrecondition();
+    try {
+        await checkIncrease();
+    } catch (e) {
+        status.increase = e.message
+    }
 }
 
-async function checkPrecondition() {
+async function checkIncrease() {
+    const before = await fetch(url + testRoomPath)
+            .then(response => response.json());
+    await fetch(url + testRoomPath + "/increase", {
+        method: 'post',
+        body: JSON.stringify({ mood: "happy" }),
+        headers: {'Content-Type': 'application/json'}
+    })
     const moods = await fetch(url + testRoomPath)
             .then(response => response.json());
-    console.log(JSON.stringify(moods));
-    if (moods.happy !== 0) {
-        return `Expected happy mood to be 0, found ${moods.happy}`;
-    }
-    return "ok";
+    assert.deepEqual(moods, {
+        ...before,
+        happy: before.happy + 1,
+    });
 }
 
 module.exports = {
