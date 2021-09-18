d4e4303b7be0ccd442c341ae73fffe3203ffc070
Pia Fåk Sunnanbo <frusunnanbo@users.noreply.github.com>
2021-09-18 12:27:49 +0200

    redefining status

--- active-monitor/server.js
+++ active-monitor/server.js
@@ -1,20 +1,15 @@
 const express = require("express");
 
-const status = require("./status.js");
 const tests = require("./run-tests.js");
 
 const app = express();
 
-const EVERY_FIVE_MINUTES = 5 * 60 * 1000;
+//const EVERY_FIVE_MINUTES = 5 * 60 * 1000;
 
 app.get("/status", function (req, res) {
-  return res.send(status.get());
-});
-
-app.get("/test-status", function (req, res) {
   return tests.runTests().then((result) => res.json(result));
 });
 
-status.checkAtInterval(EVERY_FIVE_MINUTES);
+//status.checkAtInterval(EVERY_FIVE_MINUTES);
 
 app.listen(process.env.PORT || 8081);
