1e3aa13b87d7ff87991705ac5ad1333d6ae0c520
Pia Fåk Sunnanbo <frusunnanbo@users.noreply.github.com>
2021-09-18 19:21:01 +0200

    Relative colors

--- frontend/src/components/MoodButton.js
+++ frontend/src/components/MoodButton.js
@@ -1,39 +1,40 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const StyledButton = styled.button`
-    height: 10em;
-    width: 10em;
-    border-radius: 1em;
-    margin: 2em;
-    border-width: 1px;
-    border-style: solid;
-    border-color: ${props => props.borderColor};
-    background-color: ${props => props.backgroundColor};
+  height: 10em;
+  width: 10em;
+  border-radius: 1em;
+  margin: 2em;
+  border: none;
+  background-color: ${(props) => props.backgroundColor};
 
-    &:hover {
-        background-color: ${props => props.hoverColor};
-    }
-    &:active {
-        background-color: ${props => props.activeColor};
-    }
+  &:hover {
+    background-color: ${(props) => props.hoverColor};
+  }
+  &:active {
+    background-color: ${(props) => props.activeColor};
+  }
 `;
 
 function withAlpha(color, alpha) {
-    return `rgba(${color.r}, ${color.g}, ${color.b}, ${alpha})`;
+  return `rgba(${color.r}, ${color.g}, ${color.b}, ${alpha})`;
 }
 
 function MoodButton({ text, baseColor, intensity, increaseIntensity }) {
+  const minOpacity = 0.1;
+  const backgroundOpacity = minOpacity + intensity * 0.4
 
-    return <StyledButton
-        backgroundColor={ withAlpha(baseColor, intensity * 0.01) }
-        hoverColor={ withAlpha(baseColor, intensity ? intensity * 0.03 : 0.1)}
-        activeColor={ withAlpha(baseColor, intensity ? intensity * 0.05 : 0.15)}
-        borderColor={ withAlpha(baseColor, 0.4)}
-        onClick={increaseIntensity}
-        >
-        {text}
+  return (
+    <StyledButton
+      backgroundColor={withAlpha(baseColor, backgroundOpacity)}
+      hoverColor={withAlpha(baseColor, backgroundOpacity + 0.1)}
+      activeColor={withAlpha(baseColor, backgroundOpacity + 0.2)}
+      onClick={increaseIntensity}
+    >
+      {text}
     </StyledButton>
+  );
 }
 
 export default MoodButton;
--- frontend/src/components/Room.js
+++ frontend/src/components/Room.js
@@ -1,60 +1,78 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-import * as MoodApi from "../lib/api/client"
-import { useMoods } from '../lib/hooks/useMoods';
+import * as MoodApi from "../lib/api/client";
+import { useMoods } from "../lib/hooks/useMoods";
 import MoodButton from "./MoodButton";
-
 
 const RoomDiv = styled.div`
   text-align: center;
-  min-height: 100vh;  
+  min-height: 100vh;
   display: flex;
   flex-wrap: wrap;
   align-items: center;
   align-content: center;
   justify-content: center;
-  font-size: calc(10px + 2vmin); 
+  font-size: calc(10px + 2vmin);
 `;
 
 function Room({ name }) {
-    const [moods] = useMoods(name);
+  const [moods] = useMoods(name);
 
-    const intensityIncreaser = function (mood) {
-        return function () {
-            MoodApi.increaseMood(name, mood);
-        }
-    }
+  const intensityIncreaser = function (mood) {
+    return function () {
+      MoodApi.increaseMood(name, mood);
+    };
+  };
 
-    if (!moods) {
-        return <RoomDiv></RoomDiv>;
-    }
+  if (!moods) {
+    return <RoomDiv></RoomDiv>;
+  }
 
-    return <RoomDiv>
-        <MoodButton
-            text="I'm happy"
-            baseColor={{ r: 256, b: 0, g: 256 }}
-            intensity={moods.happy}
-            increaseIntensity={intensityIncreaser("happy")}
-        />
-        <MoodButton
-            text="I feel sad"
-            baseColor={{ r: 0, b: 256, g: 0 }}
-            intensity={moods.sad}
-            increaseIntensity={intensityIncreaser("sad")}
-        />
-        <MoodButton
-            text="I'm angry"
-            baseColor={{ r: 256, b: 0, g: 0 }}
-            intensity={moods.angry}
-            increaseIntensity={intensityIncreaser("angry")}
-        />
-        <MoodButton
-            text="I feel scared"
-            baseColor={{ r: 0, b: 0, g: 256 }}
-            intensity={moods.scared}
-            increaseIntensity={intensityIncreaser("scared")}
-        />
-    </RoomDiv>;
+  const relatives = relativeIntensities(moods);
+  console.log(relatives);
+
+  return (
+    <RoomDiv>
+      <MoodButton
+        text="I'm happy"
+        baseColor={{ r: 256, b: 0, g: 256 }}
+        intensity={relatives.happy}
+        increaseIntensity={intensityIncreaser("happy")}
+      />
+      <MoodButton
+        text="I feel sad"
+        baseColor={{ r: 0, b: 256, g: 0 }}
+        intensity={relatives.sad}
+        increaseIntensity={intensityIncreaser("sad")}
+      />
+      <MoodButton
+        text="I'm angry"
+        baseColor={{ r: 256, b: 0, g: 0 }}
+        intensity={relatives.angry}
+        increaseIntensity={intensityIncreaser("angry")}
+      />
+      <MoodButton
+        text="I feel scared"
+        baseColor={{ r: 0, b: 0, g: 256 }}
+        intensity={relatives.scared}
+        increaseIntensity={intensityIncreaser("scared")}
+      />
+    </RoomDiv>
+  );
+}
+
+function relativeIntensities(moods) {
+  const sum = Object.keys(moods).reduce((acc, mood) => acc + moods[mood], 0);
+  console.log(sum);
+  if (sum === 0) {
+    return moods;
+  }
+
+  const relativeMoods = {};
+  Object.keys(moods).forEach((mood) => {
+    relativeMoods[mood] = moods[mood] / sum;
+  });
+  return relativeMoods;
 }
 
 export default Room;
