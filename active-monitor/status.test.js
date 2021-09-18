3fd848d40df4fe53b2e3bb14665904198b4b1390
Pia Fåk Sunnanbo <frusunnanbo@users.noreply.github.com>
2021-09-18 16:37:34 +0200

    Adding a check for mood increase

--- active-monitor/status.test.js
+++ active-monitor/status.test.js
@@ -4,8 +4,35 @@ const url = process.env.BASE_URL || "http://localhost:3000";
 
 describe("Moody at " + url, () => {
   it("is alive", async function () {
-    console.log(JSON.stringify(global.BASE_URL));
     const response = await fetch(url);
     expect(response).toHaveProperty("status", 200);
   });
+
+  it("can increase moods", async function () {
+    const testRoom = "test";
+    const before = await fetch(roomUrl(testRoom)).then((response) =>
+      response.json()
+    );
+    // increase twice in case autodecrease kicks in
+    await increaseMood(testRoom, "happy");
+    await increaseMood(testRoom, "happy");
+    const after = await fetch(roomUrl(testRoom)).then((response) =>
+      response.json()
+    );
+
+    expect(after.happy).toBeGreaterThan(before.happy);
+  });
 });
+
+function roomUrl(room) {
+  return url + "/api/rooms/" + room;
+}
+
+function increaseMood(room, mood) {
+  console.log(roomUrl(room) + "/increase"); 
+  return fetch(roomUrl(room) + "/increase", {
+    method: "post",
+    body: JSON.stringify({ mood: mood }),
+    headers: { "Content-Type": "application/json" },
+  });
+}
