79757aba8501ffc7c886ac5977d2c709995da78a
Pia Fåk Sunnanbo <frusunnanbo@users.noreply.github.com>
2021-09-18 10:19:56 +0200

    Prettier

--- active-monitor/server.js
+++ active-monitor/server.js
@@ -1,12 +1,12 @@
-const status = require('./status.js')
-const express = require('express');
+const status = require("./status.js");
+const express = require("express");
 
 const app = express();
 
 const EVERY_FIVE_MINUTES = 5 * 60 * 1000;
 
-app.get('/status', function (req, res) {
-    return res.send(status.get());
+app.get("/status", function (req, res) {
+  return res.send(status.get());
 });
 
 status.checkAtInterval(EVERY_FIVE_MINUTES);
--- active-monitor/status.js
+++ active-monitor/status.js
@@ -1,45 +1,47 @@
-const fetch = require('node-fetch');
-const assert = require('assert');
+const fetch = require("node-fetch");
+const assert = require("assert");
 
-const url = "https://moody.frusunnanbo.se"
+const url = "https://moody.frusunnanbo.se";
 const testRoomPath = "/api/rooms/test";
 
 const status = {
-    increase: "ok",
-    autodecrease: "ok",
+  increase: "ok",
+  autodecrease: "ok",
 };
 
 function checkAtInterval(milliseconds) {
-    checkStatus()
-    setInterval(checkStatus, milliseconds);
+  checkStatus();
+  setInterval(checkStatus, milliseconds);
 }
 
 async function checkStatus() {
-    console.log("Checking status");
-    try {
-        await checkIncrease();
-    } catch (e) {
-        status.increase = e.message
-    }
+  console.log("Checking status");
+  try {
+    await checkIncrease();
+  } catch (e) {
+    status.increase = e.message;
+  }
 }
 
 async function checkIncrease() {
-    const before = await fetch(url + testRoomPath)
-            .then(response => response.json());
-    await fetch(url + testRoomPath + "/increase", {
-        method: 'post',
-        body: JSON.stringify({ mood: "happy" }),
-        headers: {'Content-Type': 'application/json'}
-    })
-    const moods = await fetch(url + testRoomPath)
-            .then(response => response.json());
-    assert.deepEqual(moods, {
-        ...before,
-        happy: before.happy + 1,
-    });
+  const before = await fetch(url + testRoomPath).then((response) =>
+    response.json()
+  );
+  await fetch(url + testRoomPath + "/increase", {
+    method: "post",
+    body: JSON.stringify({ mood: "happy" }),
+    headers: { "Content-Type": "application/json" },
+  });
+  const moods = await fetch(url + testRoomPath).then((response) =>
+    response.json()
+  );
+  assert.deepEqual(moods, {
+    ...before,
+    happy: before.happy + 1,
+  });
 }
 
 module.exports = {
-    checkAtInterval,
-    get: () => status
+  checkAtInterval,
+  get: () => status,
 };
